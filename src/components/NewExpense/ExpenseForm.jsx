1e76e377d84e33b1c17ea2483d9018e56ee77127
Keshav Sharma <keshav2743@gmail.com>
2022-11-12 17:14:07 +0530

    final changes

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -44,7 +44,7 @@ const ExpenseForm = (props) => {
         event.preventDefault();
         const expenseData = {
             title: userInput.title,
-            amount: userInput.amount,
+            amount: +userInput.amount,
             date: new Date(userInput.date)
         };
         // console.log(expenseData);
